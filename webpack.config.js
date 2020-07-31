e98a1b231b9cb17aa6b3cc955b232e894a4fce89
Will Clardy <wclardy@kmbs.konicaminolta.us>
2020-07-30 23:49:22 -0400

    Bump dependencies; Add standard; Drop webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,26 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
 
 module.exports = {
-    mode: 'production',
-    entry: "./src/main.js",
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/assets/',
-        filename: "fryte.min.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                ],
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader',
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'example'),
-        compress: true,
-        inline: true,
-    },
-};
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: 'fryte.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/i,
+        loader: 'svg-inline-loader'
+      }
+    ]
+  },
+  optimization: {
+    minimize: true
+  }
+}
